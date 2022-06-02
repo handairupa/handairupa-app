7ce30f5eecdf4ebb7d30f7b665920e9921724e09
Gracia Kanna <99242649+kannagracia@users.noreply.github.com>
2022-06-02 14:47:44 +0700

    Added Navigation

---
+++ app/src/main/java/com/example/handairupaapps/ui/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.example.handairupaapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.handairupaapps.R
+import com.example.handairupaapps.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_journal, R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/example/handairupaapps/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/handairupaapps/ui/settings/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.handairupaapps.ui.journal
+package com.example.handairupaapps.ui.settings
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -7,11 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.example.handairupaapps.databinding.FragmentJournalBinding
+import com.example.handairupaapps.R
+import com.example.handairupaapps.databinding.FragmentSettingsBinding
 
-class JournalFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentJournalBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     private val binding get() = _binding!!
 
@@ -20,14 +21,14 @@ class JournalFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val journalViewModel =
-            ViewModelProvider(this)[JournalViewModel::class.java]
+        val settingsViewModel =
+            ViewModelProvider(this)[SettingsViewModel::class.java]
 
-        _binding = FragmentJournalBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.journalFragment
-        journalViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.settingsFragment
+        settingsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
